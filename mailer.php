b98957ae0fd4e2398b6d4828ac0ece1bbc195ca8
abdullahalkannas <rafabire321@gmail.com>
2019-11-26 09:03:38 +0200

    still trynna fix the mailer

--- mailer.php
+++ mailer.php
@@ -9,7 +9,6 @@
         $name = strip_tags(trim($_POST["name"]));
 				$name = str_replace(array("\r","\n"),array(" "," "),$name);
         $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
-        // $cont_subject = trim($_POST["subject"]);
         $message = trim($_POST["message"]);
 
         // Check that data was sent to the mailer.
@@ -30,7 +29,6 @@
         // Build the email content.
         $email_content = "Name: $name\n";
         $email_content .= "Email: $email\n\n";
-        // $email_content .= "Subject: $cont_subject\n";
         $email_content .= "Message:\n$message\n";
 
         // Build the email headers.
