ef77d12c903f7e35a5dfb55df67dffa2d1bc4de7
abdullahalkannas <rafabire321@gmail.com>
2019-11-26 06:03:09 +0200

    updated mailer.php

--- mailer.php
+++ mailer.php
@@ -22,7 +22,7 @@
 
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "contact@markups.io";
+        $recipient = "bughuntergirl@gmail.com";
 
         // Set the email subject.
         $subject = "New contact from $name";
