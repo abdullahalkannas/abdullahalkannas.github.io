10be1b3249a1b5ece1837597ccae1cf08b30ce55
abdullahalkannas <rafabire321@gmail.com>
2019-12-03 21:30:33 +0200

    added the rest of the dawrat and the load more button

---
+++ load-more-button.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $(".content").slice(0, 8).show();
+    $("#loadMore").on("click", function(e){
+      e.preventDefault();
+      $(".content:hidden").slice(0, 4).slideDown();
+      if($(".content:hidden").length == 0) {
+        $("#loadMore").text("").addClass("noContent");
+      }
+    });
+    
+  })
